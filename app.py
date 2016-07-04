93826c4d87d0d7b474fd2a7e8103bc4b86ad7dae
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-07-04 01:27:40 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
