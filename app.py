d47683fc4ee4b7ec183068dc4048f55ba600f445
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-07-04 01:44:49 -0500

    basic behavior for turn_on and turn_off{

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from telnetlib import Telnet
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
-
 @app.route('/')
 def home():
-    """Render website's home page."""
     return render_template('home.html')
+
+@app.route('/turn_on', methods = ['GET'])
+def turn_on():
+    tel = Telnet('foxnegro.zapto.org')
+    tel.write(b'X\n')
+    tel.close()
+
+@app.route('/turn_off', methods = ['GET'])
+def turn_off():
+    tel = Telnet('foxnegro.zapto.org')
+    tel.write(b'Y\n')
+    tel.close()
 
 @app.after_request
 def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=600'
     return response
@@ -24,9 +31,7 @@ def add_header(response):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    """Custom 404 page."""
     return render_template('404.html'), 404
-
 
 if __name__ == '__main__':
     app.run(debug=True)
